92b610ce95fa38423470468b404287e6a1349b48
Nguyen Toan <ngoctoan06011998>
2025-06-13 15:32:00 +0700

    update: navigate home

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>XSocial</title>
+</head>
+
+<body>
+
+</body>
+<script>
+  window.onload = function() {
+    window.location.assign(`${window.location.href}api/documentation`);
+  }
+</script>
+
+</html>
